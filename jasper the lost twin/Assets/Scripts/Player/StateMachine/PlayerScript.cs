f7b95c739fec11f757a86fc5b13a2ef54a96e788
Andrei-George Lichi <lichiag21@gmail.com>
2024-02-19 17:32:14 +0100

    Player state methods added

--- jasper the lost twin/Assets/Scripts/Player/StateMachine/PlayerScript.cs
+++ jasper the lost twin/Assets/Scripts/Player/StateMachine/PlayerScript.cs
@@ -1,18 +1,9 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Animator Anim { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- jasper the lost twin/Assets/Scripts/Player/StateMachine/PlayerState.cs
+++ jasper the lost twin/Assets/Scripts/Player/StateMachine/PlayerState.cs
@@ -5,12 +5,51 @@ using UnityEngine;
 public class PlayerState
 {
 	protected PlayerScript player;
-	protected PlayerStateMachine sstateMachine;
+	protected PlayerStateMachine stateMachine;
 	protected PlayerData playerData;
 	protected bool isAnimationFinished;
 	protected float startTime;
 	protected string animBoolName;
 	
-	public PlayerState(Player player)
+	public PlayerState(PlayerScript player, PlayerStateMachine stateMachine, PlayerData playerDate, string animBoolName)
+	{
+	 this.player = player;
+	 this.stateMachine = stateMachine;
+	 this.playerData = playerDate;
+	 this.animBoolName = animBoolName;
+	}
+	
+	public virtual void Enter() 
+	{
+		Validate();	
+		player.Anim.SetBool(animBoolName, true);
+		startTime = Time.time;
+		isAnimationFinished = false;
+		Debug.Log($"State:{animBoolName}");
+	}
+	
+	public virtual void Exit()
+	{
+		player.Anim.SetBool(animBoolName, false);
+	}
+	
+	public virtual void LogicUpdate() 
+	{
+		
+	}
+	
+	public virtual void PhysicsUpdate()
+	{
+		Validate();	
+	}
+	
+	public virtual void Validate()
+	{
+		
+	}
+	
+	public virtual void AnimationTrigger() {}
+	
+	public virtual void AnimationFinishTrigger() => isAnimationFinished = true;
 	
 }
