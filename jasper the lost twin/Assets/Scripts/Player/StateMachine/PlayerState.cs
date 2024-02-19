5d79228d2f2e2e3f143088c30f7c5f6112c18186
Andrei-George Lichi <lichiag21@gmail.com>
2024-02-19 17:22:45 +0100

    Player state machine added

---
+++ jasper the lost twin/Assets/Scripts/Player/StateMachine/PlayerState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+	protected PlayerScript player;
+	protected PlayerStateMachine sstateMachine;
+	protected PlayerData playerData;
+	protected bool isAnimationFinished;
+	protected float startTime;
+	protected string animBoolName;
+	
+	public PlayerState(Player player)
+	
+}
