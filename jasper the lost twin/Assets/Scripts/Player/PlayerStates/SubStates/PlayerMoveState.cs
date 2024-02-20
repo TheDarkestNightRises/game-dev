6cb3e4880b929e34f17d0e9ed5f5878c3412f0c7
Andrei-George Lichi <lichiag21@gmail.com>
2024-02-20 13:20:12 +0100

    player move state added

---
+++ jasper the lost twin/Assets/Scripts/Player/PlayerStates/SubStates/PlayerMoveState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveState : PlayerGroundState
+{
+
+    public PlayerMoveState(PlayerScript player, PlayerStateMachine playerStateMachine, PlayerData playerData, string animBoolName) : base(player, playerStateMachine, playerData, animBoolName)
+    {
+
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        player.SetVelocityX(playerData.movementVelocity * xInput);
+
+        if (xInput == 0)
+        {
+            stateMachine.ChangeState(player.IdleState);
+        }
+    }
+}
--- jasper the lost twin/Assets/Scripts/Player/StateMachine/PlayerData.cs
+++ jasper the lost twin/Assets/Scripts/Player/StateMachine/PlayerData.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData : MonoBehaviour
+public class PlayerData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float movementVelocity = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
