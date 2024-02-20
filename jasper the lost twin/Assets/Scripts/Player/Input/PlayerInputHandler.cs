cfa19052ad6dbdae796a3a4ccd584a2e6e865c1f
Oriana11 <rubyjpy@gmail.com>
2024-02-20 12:55:14 +0100

    Added Character inputs

---
+++ jasper the lost twin/Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+	
+	public Vector2 MovementInput { get; set; }
+	
+	
+	public void OnMoveInput(InputAction.CallbackContext context)
+	{
+		MovementInput = context.ReadValue<Vector2>();
+		Debug.Log(MovementInput);
+	}
+	
+	public void OnJumpInput(InputAction.CallbackContext context)
+	{
+		if(context.started)
+			Debug.Log("Jumped");
+		
+	}
+}
