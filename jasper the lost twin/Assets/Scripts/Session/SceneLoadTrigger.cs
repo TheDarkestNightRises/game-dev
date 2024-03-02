51aa0a0fa08151b2fc6c6c527c15f48a859981a6
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-03-02 15:39:21 +0100

    Fixed issue with concurency and improved scene trigger

--- jasper the lost twin/Assets/Scripts/Session/SceneLoadTrigger.cs
+++ jasper the lost twin/Assets/Scripts/Session/SceneLoadTrigger.cs
@@ -5,26 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoadTrigger : MonoBehaviour
 {
-	[SerializeField] private string _sceneToLoad;
-	[SerializeField] private string _sceneToUnload;
+	[SerializeField] private SceneField[] _sceneToLoad;
+	[SerializeField] private SceneField[] _sceneToUnload;
 	
 	protected void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			Debug.Log("TRIGGER SCENE LOAD");
-			LoadScene();
-			UnloadScene();
+			LoadScenes();
+			UnloadScenes();
 		}
 	}
 	
-	private void LoadScene()
-	{
-		SceneManager.LoadSceneAsync(_sceneToLoad, LoadSceneMode.Additive);
+	private void LoadScenes()
+	{		
+		foreach (SceneField scene in _sceneToLoad)
+		{
+			if (!IsSceneLoaded(scene))
+			{
+				LoadSceneAsync(scene);
+			}
+		}
 	}
 	
-	private void UnloadScene()
+	private bool IsSceneLoaded(SceneField scene)
 	{
-		SceneManager.UnloadSceneAsync(_sceneToUnload);
+		foreach (Scene loadedScene in SceneManager.GetAllScenes())
+		{
+			if (scene.SceneName == loadedScene.name)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private void LoadSceneAsync(SceneField scene)
+	{
+		SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+	}
+
+	private void UnloadScenes()
+	{
+		foreach (SceneField scene in _sceneToUnload)
+		{
+			UnloadSceneAsync(scene);
+		}
+	}
+	
+	private void UnloadSceneAsync(SceneField scene)
+	{
+		SceneManager.UnloadSceneAsync(scene);
 	}
 }
