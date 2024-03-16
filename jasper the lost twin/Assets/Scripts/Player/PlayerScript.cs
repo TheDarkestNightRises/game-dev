750761b5a8c790f6f735f547157331d73352b0b0
Oriana11 <rubyjpy@gmail.com>
2024-03-16 13:40:05 +0100

    Added spikes to the level

--- jasper the lost twin/Assets/Scripts/Player/PlayerScript.cs
+++ jasper the lost twin/Assets/Scripts/Player/PlayerScript.cs
@@ -21,6 +21,11 @@ public class PlayerScript : MonoBehaviour
 	private PlayerData playerData;
 	public Rigidbody2D RB { get; set; }
 	private BoxCollider2D myFeetCollider;
+	private CapsuleCollider2D myBodyCollider;
+	
+	[SerializeField]
+	Vector2 deathKick = new Vector2(5f, 5f);
+	
 	#endregion
 	
 	#region Particles
@@ -30,6 +35,8 @@ public class PlayerScript : MonoBehaviour
 	public Vector2 CurrentVelocity { get; set; }
 	public int FacingDirection { get; set; }
 	private Vector2 workspace;
+
+	
 
 	public void Awake()
 	{
@@ -41,9 +48,12 @@ public class PlayerScript : MonoBehaviour
 		LandState = new PlayerLandState(this, StateMachine, playerData, "land");
 	}
 	
+	bool isAlive = true;
+	
 	private void Start()
 	{
 		myFeetCollider = GetComponent<BoxCollider2D>();
+		myBodyCollider = GetComponent<CapsuleCollider2D>();
 		Anim = GetComponent<Animator>();
 		InputHandler = GetComponent<PlayerInputHandler>();
 		RB = GetComponent<Rigidbody2D>();
@@ -53,13 +63,18 @@ public class PlayerScript : MonoBehaviour
 	
 	private void Update()
 	{
+		if (!isAlive) return;
 		CurrentVelocity = RB.velocity;
 		StateMachine.CurrentState.LogicUpdate();
+		Die();
+	
 	}
 	
 	private void FixedUpdate()
 	{
+		if (!isAlive) return;
 		StateMachine.CurrentState.PhysicsUpdate();
+		
 	}
 	
 	public void SetVelocityX(float velocity)
@@ -74,6 +89,22 @@ public class PlayerScript : MonoBehaviour
 		workspace.Set(CurrentVelocity.x, velocity);
 		RB.velocity = workspace;
 		CurrentVelocity = workspace;
+	}
+	
+	// Player dies method
+	void Die()
+	{
+		if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Hazards")))
+		{
+			isAlive = false;
+			Anim.SetTrigger("die");
+			Invoke(nameof(RemovePhysics), 1f);
+		}
+	}
+	
+	private void RemovePhysics()
+	{
+		RB.simulated = false;
 	}
 	
 	public void Flip()
@@ -104,3 +135,16 @@ public class PlayerScript : MonoBehaviour
 	
 	private void AnimationFinishedTrigger() => StateMachine.CurrentState.AnimationFinishTrigger();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
