082dc0e84242f95ddbff98d2c5b551a0b8b596a2
Andrei-George Lichi <lichiag21@gmail.com>
2024-03-17 15:06:13 +0100

    One way platform fixed

--- jasper the lost twin/Assets/Scripts/Platform/OneWayPlatform.cs
+++ jasper the lost twin/Assets/Scripts/Platform/OneWayPlatform.cs
@@ -4,23 +4,53 @@ using UnityEngine;
 
 public class OneWayPlatform : MonoBehaviour
 {
-	[SerializeField]
-	public LayerMask whatIsPlayer;
-	[SerializeField]
-	public LayerMask whatIsGround;
+	private GameObject currentOneWayPlatform;
 	private PlayerInputHandler playerInputHandler;
+	private BoxCollider2D playerCollider;
+	private CapsuleCollider2D playerCapsuleCollider;
 	
 	public void Start() 
 	{
+		playerCollider = GetComponent<BoxCollider2D>();
+		playerCapsuleCollider = GetComponent<CapsuleCollider2D>();
 		playerInputHandler = GetComponent<PlayerInputHandler>();
 	}
 	
 	public void Update()
-    {
-	    if(playerInputHandler.InputY < 0)
-		   {
-		    Physics2D.IgnoreLayerCollision(whatIsPlayer,whatIsGround,true);
-		   }
-	    Physics2D.IgnoreLayerCollision(whatIsPlayer,whatIsGround,false);
-    }
+	{
+		if(playerInputHandler.InputY != 0)
+		{
+			if(currentOneWayPlatform != null)
+			{
+				StartCoroutine(DisableCollision());
+			}
+		}
+	}
+    
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag("OneWayPlatform"))
+		{
+			currentOneWayPlatform = collision.gameObject;	 
+		}
+	}
+    
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag("OneWayPlatform"))
+		{
+			currentOneWayPlatform = null;
+		}	
+	}
+	
+	private IEnumerator DisableCollision()
+	{
+		BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+		Physics2D.IgnoreCollision(playerCollider, platformCollider,true);
+		Physics2D.IgnoreCollision(playerCapsuleCollider, platformCollider,true);
+		yield return new WaitForSeconds(1f);
+		Physics2D.IgnoreCollision(playerCollider,platformCollider,false);	
+		Physics2D.IgnoreCollision(playerCapsuleCollider,platformCollider,false);	
+
+	}
 }
