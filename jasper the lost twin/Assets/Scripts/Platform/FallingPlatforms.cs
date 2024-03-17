f5a8ff1db1b985ef8bbc8754ec64733636a6142f
Andrei-George Lichi <lichiag21@gmail.com>
2024-03-17 13:15:49 +0100

    Added falling platform script

--- jasper the lost twin/Assets/Scripts/Platform/FallingPlatforms.cs
+++ jasper the lost twin/Assets/Scripts/Platform/FallingPlatforms.cs
@@ -1,18 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FallingPlatforms : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	public float fallDelay = 1f;
+	[SerializeField]
+	public float destroyDelay = 2f;
+	[SerializeField]
+	private Rigidbody2D rb;
+	
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag("Player")) 
+		{
+			StartCoroutine(Fall());
+		}
+	}	
+	private IEnumerator Fall() 
+	{
+		yield return WaitForSeconds(fallDelay);
+		rb.bodyType = RigidbodyType2D.Dynamic;
+		Destroy(gameObject, destroyDelay);
+	}
+	
 }
+  
