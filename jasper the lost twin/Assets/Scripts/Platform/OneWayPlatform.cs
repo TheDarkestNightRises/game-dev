8a1c3b168eb18c1e6680b0e3374e768715c8c1ed
Andrei-George Lichi <lichiag21@gmail.com>
2024-03-17 13:52:23 +0100

    One way platform added

---
+++ jasper the lost twin/Assets/Scripts/Platform/OneWayPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlatform : MonoBehaviour
+{
+	[SerializeField]
+	public LayerMask whatIsPlayer;
+	[SerializeField]
+	public LayerMask whatIsGround;
+	private PlayerInputHandler playerInputHandler;
+	
+	public void Start() 
+	{
+		playerInputHandler = GetComponent<PlayerInputHandler>();
+	}
+	
+	public void Update()
+    {
+	    if(playerInputHandler.InputY < 0)
+		   {
+		    Physics2D.IgnoreLayerCollision(whatIsPlayer,whatIsGround,true);
+		   }
+	    Physics2D.IgnoreLayerCollision(whatIsPlayer,whatIsGround,false);
+    }
+}
