6be3b7968a48589380f4891e9f9de5fd8cb7a7eb
Andrei-George Lichi <lichiag21@gmail.com>
2024-04-13 21:01:03 +0200

    Icy platforms added

---
+++ jasper the lost twin/Assets/Scripts/Platform/IcyPlatforms.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcyPlatforms : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	[SerializeField]
+	private float SlipperyFriction = 0.2;
+	private float currentPlayerFriction;
+
+	public void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Slip(other.gameObject);
+		}
+	}
+	
+	private void Slip(GameObject other)
+	{
+		var player = other.GetComponent<PlayerScript>();
+		currentPlayerFriction = player.playerData.friction;
+		player.playerData.friction = SlipperyFriction;
+	}
+	
+	void OnCollisionExit2D(Collision2D other)
+	{
+		print("No longer in contact with " + other.transform.name);
+		if (other.gameObject.CompareTag("Player"))
+		{
+			var player = other.gameObject.GetComponent<PlayerScript>();
+			player.playerData.friction = currentPlayerFriction;
+		}
+	}
+	
+	
+}
