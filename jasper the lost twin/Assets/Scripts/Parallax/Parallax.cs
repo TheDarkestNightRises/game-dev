4214658efa0a8771e2754318316ff41548cddfa8
Andrei-George Lichi <lichiag21@gmail.com>
2024-04-18 21:20:41 +0200

    Parallax added

---
+++ jasper the lost twin/Assets/Scripts/Parallax/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+	private float length;
+	private float startPos;
+	public GameObject camera;
+	public float parallaxEffect;
+	
+    void Start()
+    {
+	    startPos =	transform.position.x;
+	    length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+	    float distance = (camera.transform.position.x * parallaxEffect);
+	    transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
