7912d6f91671af79d15cd0e9bcf636d376023976
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-05-16 23:27:54 +0200

    Layer physics change

---
+++ jasper the lost twin/Assets/Scripts/Combat/Damageable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour, IDamageable
+{
+	[SerializeField]
+	private float _maxHealth;
+	
+	public void Damage(DamageData damage){}
+}
