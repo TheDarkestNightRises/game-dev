dc5aac153aea9e479112a7e7411130d9e254b997
Oriana11 <rubyjpy@gmail.com>
2024-05-17 20:53:05 +0200

    Added Timer Feature

---
+++ jasper the lost twin/Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI timerText;
+	[SerializeField] float remainingTime;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (remainingTime > 0)
+		{
+			remainingTime -= Time.deltaTime;
+		}
+		else if (remainingTime < 0)
+		{
+			remainingTime = 0;
+		}
+
+		int minutes = Mathf.FloorToInt(remainingTime / 60);
+		int seconds = Mathf.FloorToInt(remainingTime % 60);
+		timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+		// Change the color to red when remaining time is less than 10 seconds
+		if (remainingTime <= 10f)
+		{
+			timerText.color = Color.red;
+		}
+		else
+		{
+			timerText.color = Color.white;
+		}
+	}
+}
