f4e7b9ace5eeecf6ea5f9433259e91af618a781e
Oriana11 <rubyjpy@gmail.com>
2024-05-17 21:03:59 +0200

    Update timer count and color

--- jasper the lost twin/Assets/Scripts/Timer/Timer.cs
+++ jasper the lost twin/Assets/Scripts/Timer/Timer.cs
@@ -8,30 +8,23 @@ public class Timer : MonoBehaviour
 	[SerializeField] TextMeshProUGUI timerText;
 	[SerializeField] float remainingTime;
 
+
 	// Update is called once per frame
 	void Update()
 	{
-		if (remainingTime > 0)
+		if (remainingTime < 60)
 		{
-			remainingTime -= Time.deltaTime;
+			remainingTime += Time.deltaTime;
 		}
-		else if (remainingTime < 0)
+		else if (remainingTime > 60)
 		{
-			remainingTime = 0;
+			remainingTime = 60;
 		}
 
 		int minutes = Mathf.FloorToInt(remainingTime / 60);
 		int seconds = Mathf.FloorToInt(remainingTime % 60);
 		timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
-		// Change the color to red when remaining time is less than 10 seconds
-		if (remainingTime <= 10f)
-		{
-			timerText.color = Color.red;
-		}
-		else
-		{
-			timerText.color = Color.white;
-		}
+	
 	}
 }
