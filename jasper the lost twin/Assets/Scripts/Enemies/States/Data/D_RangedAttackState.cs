63a9aa57fcfc1e977864d8c0a1075b29e94cbc23
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-05-19 01:42:48 +0200

    Finished archer and collision

---
+++ jasper the lost twin/Assets/Scripts/Enemies/States/Data/D_RangedAttackState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newRangeData", menuName = "Data/Enemy/Ranged attack state")]
+public class D_RangedAttackState : ScriptableObject
+{
+	public GameObject projectile;
+	public float projectileDamage;
+	public float projectileSpeed;
+	public float projectileTravel;
+}
---
+++ jasper the lost twin/Assets/Scripts/Enemies/States/Projectile.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float speed;
+    private float travelDistance;
+    private float xStartPos;
+	private float damage; 
+	
+    [SerializeField]
+    private float gravity;
+    [SerializeField]
+    private float damageRadius;
+
+    private Rigidbody2D rb;
+
+	private bool isGravityOn = false;
+    private bool hasHitGround;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+    [SerializeField]
+    private LayerMask whatIsPlayer;
+    [SerializeField]
+    private Transform damagePosition;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.gravityScale = 0.0f;
+	    rb.velocity = -transform.right * speed;
+
+        isGravityOn = false;
+
+        xStartPos = transform.position.x;
+    }
+
+    private void Update()
+    {
+        if (!hasHitGround)
+        {
+            if (isGravityOn)
+            {
+                float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!hasHitGround)
+        {
+            Collider2D damageHit = Physics2D.OverlapCircle(damagePosition.position, damageRadius, whatIsPlayer);
+            Collider2D groundHit = Physics2D.OverlapCircle(damagePosition.position, damageRadius, whatIsGround);
+
+            if (damageHit)
+            {
+	            var damageable = damageHit.GetComponent<IDamageable>();
+	            if (damageable != null)
+	            {
+		            var damageData = new DamageData(damage, this.gameObject);
+		            damageable.Damage(damageData);
+		            Destroy(gameObject); 
+	            }
+            }
+
+            if (groundHit)
+            {
+                hasHitGround = true;
+                rb.gravityScale = 0f;
+	            rb.velocity = Vector2.zero;
+	            Destroy(gameObject); 
+            }
+
+
+            if (Mathf.Abs(xStartPos - transform.position.x) >= travelDistance && !isGravityOn)
+            {
+                isGravityOn = true;
+                rb.gravityScale = gravity;
+            }
+        }
+    }
+
+    public void FireProjectile(float speed, float travelDistance, float damage)
+    {
+        this.speed = speed;
+        this.travelDistance = travelDistance;
+	    this.damage = damage;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(damagePosition.position, damageRadius);
+    }
+    
+}
+
