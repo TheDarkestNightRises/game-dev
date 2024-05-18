37294f774383f940f76808dad0c15ec6d23f798b
Oriana11 <rubyjpy@gmail.com>
2024-05-18 21:15:46 +0200

    Added GunTrap script

---
+++ jasper the lost twin/Assets/Scripts/Traps/GunTrap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunTrap : MonoBehaviour
+{
+	[SerializeField] public float attackCooldown;
+	[SerializeField] public Transform firePoint;
+	[SerializeField] public GameObject[] fireballs;
+	private float cooldownTimer;
+	
+	private void Attack()
+	{
+		cooldownTimer = 0;
+		
+		fireballs[FindFireball()].transform.position = firePoint.position;
+		//fireballs[FindFireball()].GetComponent<EnemyProjectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+	}
+	
+	private int FindFireball()
+	{
+		for(int i = 0; i < fireballs.Length; i++ )
+		{
+			if(!fireballs[i].activeInHierarchy)
+				return i;
+		}
+		return 0;
+	}
+	
+	private void Update()
+	{
+		cooldownTimer += Time.deltaTime;
+		if(cooldownTimer >= attackCooldown )
+		{
+			Attack();
+		}
+	}	
+}
