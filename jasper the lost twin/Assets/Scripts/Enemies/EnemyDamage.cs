be4ff1c580a1a0afd2e122a23fd944954bc2afea
Oriana11 <rubyjpy@gmail.com>
2024-05-19 00:29:50 +0200

    Added entity damage

---
+++ jasper the lost twin/Assets/Scripts/Enemies/EnemyDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+	[SerializeField] public float damage;
+   
+	// Sent when another object enters a trigger collider attached to this object (2D physics only).
+	protected void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")			
+		{
+			var damageData = new DamageData(damage, this.gameObject);
+			other.GetComponent<IDamageable>().Damage(damageData);
+		}
+	}
+}
