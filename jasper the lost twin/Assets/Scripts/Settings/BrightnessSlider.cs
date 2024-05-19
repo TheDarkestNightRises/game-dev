e0b805eb4b80ec3fad76e9ef154803c6d6e70b52
Andrei-George Lichi <lichiag21@gmail.com>
2024-05-19 16:38:21 +0200

    Sliders adjusted

---
+++ jasper the lost twin/Assets/Scripts/Settings/BrightnessSlider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrightnessSlider : MonoBehaviour
+{
+	public Slider slider; 
+
+	void Start()
+	{
+		slider.value = PlayerPrefs.GetFloat("BrightnessSliderValue", 0.5f);
+		slider.onValueChanged.AddListener(delegate { SaveSliderValue(); });
+	}
+
+	void SaveSliderValue()
+	{
+		PlayerPrefs.SetFloat("BrightnessSliderValue", slider.value);
+	}
+}
---
+++ jasper the lost twin/Assets/Scripts/Settings/VolumeSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+	
+		public Slider slider; 
+
+		void Start()
+     	{
+	     	slider.value = PlayerPrefs.GetFloat("VolumeSliderValue", 0.5f);
+			slider.onValueChanged.AddListener(delegate { SaveSliderValue(); });
+		}
+
+		void SaveSliderValue()
+		{
+			PlayerPrefs.SetFloat("VolumeSliderValue", slider.value);
+		}
+}
