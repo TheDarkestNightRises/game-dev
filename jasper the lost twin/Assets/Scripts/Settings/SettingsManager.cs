ef419d7cd26662f450d1889190efdb2b921692a1
Andrei-George Lichi <lichiag21@gmail.com>
2024-05-19 20:34:11 +0200

    fixed settings manager

---
+++ jasper the lost twin/Assets/Scripts/Settings/SettingsManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+
+	[SerializeField] private SceneField _mainMenu;
+
+	public void BackToMainMenu() 
+	{
+		SceneManager.LoadSceneAsync(_mainMenu);
+	}
+}
