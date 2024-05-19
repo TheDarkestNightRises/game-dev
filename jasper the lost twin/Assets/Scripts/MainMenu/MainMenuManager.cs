ac0fe0cc1eadff13ae25b1916ed074a51575efbf
Andrei-George Lichi <lichiag21@gmail.com>
2024-05-19 16:02:25 +0200

    Added volume slider

--- jasper the lost twin/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ jasper the lost twin/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -8,11 +8,22 @@ public class MainMenuManager : MonoBehaviour
 {
 	[SerializeField] private SceneField _firstLevel;
 	[SerializeField] private SceneField _persistentScene;
+	[SerializeField] private SceneField _settingsMenu;
 	
 	public void StartGame()
 	{
 		SceneManager.LoadSceneAsync(_persistentScene);
 		SceneManager.LoadSceneAsync(_firstLevel, LoadSceneMode.Additive);
 	}	
+	
+	public void OpenSettings()
+	{
+		SceneManager.LoadSceneAsync(_settingsMenu);
+	}
+	
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
 
 }
