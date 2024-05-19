b84d8bcbbd47e9a10aa457cf138f58030bbae766
Andrei-George Lichi <lichiag21@gmail.com>
2024-05-19 22:36:03 +0200

    Resume menu created

---
+++ jasper the lost twin/Assets/Scripts/UI/PauseMenu/PauseMenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+	//[SerializeField] private SceneField _firstLevel;
+	//[SerializeField] private SceneField _persistentScene;
+	//[SerializeField] private SceneField _settingsMenu;
+	//[SerializeField] private SceneField _mainMenu;
+	//public AudioClip mainMenuMusic;
+	
+	
+	//void Start()
+	//{
+	//	AudioManager.instance.PlayMusic(mainMenuMusic);
+	//}
+	
+	
+	//public void StartGame()
+	//{
+	//	AudioManager.instance.StopMusic();
+	//	SceneManager.LoadSceneAsync(_persistentScene);
+	//	SceneManager.LoadSceneAsync(_firstLevel, LoadSceneMode.Additive);
+	//}	
+	
+	//public void OpenSettings()
+	//{
+	//	SceneManager.LoadSceneAsync(_settingsMenu);
+	//}
+	
+	//public void QuitGame()
+	//{
+	//	Application.Quit();
+	//}
+	
+	//public void BackToMainMenu() 
+	//{
+	//	SceneManager.LoadSceneAsync(_mainMenu);
+	//}
+
+}
