bfba86d2858970ab9a67d4dd6ff77635b6c35dae
Oriana11 <rubyjpy@gmail.com>
2024-05-20 17:55:39 +0200

    Added pendulum script

---
+++ jasper the lost twin/Assets/Scripts/Traps/Pendulum.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+	Rigidbody2D rb2d;
+	
+	public float moveSpeed;
+	public float leftAngle;
+	public float rightAngle;
+	
+	bool movingClockwise;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	    rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    Debug.Log(transform.rotation.z);
+	    rb2d.angularVelocity = moveSpeed;
+	    
+    }
+}
