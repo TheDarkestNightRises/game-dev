b03dc5a084aaac05e899ff949261031426bdf864
Oriana11 <rubyjpy@gmail.com>
2024-05-20 18:16:50 +0200

    Completed script for pendulum

--- jasper the lost twin/Assets/Scripts/Traps/Pendulum.cs
+++ jasper the lost twin/Assets/Scripts/Traps/Pendulum.cs
@@ -17,13 +17,37 @@ public class Pendulum : MonoBehaviour
     void Start()
     {
 	    rb2d = GetComponent<Rigidbody2D>();
+	    movingClockwise = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-	    Debug.Log(transform.rotation.z);
-	    rb2d.angularVelocity = moveSpeed;
-	    
+	    Move();
     }
+    
+	public void ChangeMoveDir()
+	{
+		if(transform.rotation.z > rightAngle)
+		{
+			movingClockwise = false;
+		}
+		if(transform.rotation.z < leftAngle)
+		{
+			movingClockwise = true;
+		}
+	}
+	public void Move()
+	{
+		ChangeMoveDir();
+		if(movingClockwise)
+		{
+			rb2d.angularVelocity = moveSpeed;
+		}
+		
+		if(!movingClockwise)
+		{
+			rb2d.angularVelocity = - 1 * moveSpeed; 
+		}
+	}
 }
