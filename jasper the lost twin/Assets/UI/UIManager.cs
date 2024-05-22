bcbb04525b5e438cd93590c2786ed5962b148137
Oriana11 <rubyjpy@gmail.com>
2024-05-22 20:08:28 +0200

    Added Death UI

---
+++ jasper the lost twin/Assets/UI/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	[SerializeField] GameObject deathPanel;
+	
+	public void ShowDeathUi()
+	{
+		deathPanel.SetActive(true);
+	}
+}
