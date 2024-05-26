942652154f843b70130e5fbac7cc89f3ff7749d1
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-05-26 22:24:02 +0200

    Restructured folders

---
+++ jasper the lost twin/Assets/Scripts/UI/Parallax/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+	private float length;
+	private float startPos;
+	private string cameraTag = "MainCamera";
+	public float parallaxEffect;
+	private Camera camera;
+	
+    void Start()
+    {
+	    GameObject cameraObject = GameObject.FindWithTag(cameraTag);
+	    camera = cameraObject.GetComponent<Camera>();
+	    startPos =	transform.position.x;
+	    length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+	    float distance = camera.transform.position.x * parallaxEffect;
+	    transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
