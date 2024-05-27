cd1bdc210d7363864facc8215060e735b55ba8bc
Oriana11 <rubyjpy@gmail.com>
2024-05-27 11:05:06 +0200

    Added door

--- jasper the lost twin/Assets/Scripts/Doors and Key/Door.cs
+++ jasper the lost twin/Assets/Scripts/Doors and Key/Door.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+
+    private PlayerScript thePlayer;
+
+    public SpriteRenderer theSR;
+
+    public Sprite doorOpenSprite;
+
+    public bool doorOpen, waitingToOpen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        thePlayer = FindObjectOfType<PlayerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(waitingToOpen)
+        {
+            if (Vector3.Distance(thePlayer.followingKey.transform.position, transform.position) < 0.1f)
+            {
+                waitingToOpen = false;
+
+                doorOpen = true;
+
+                theSR.sprite = doorOpenSprite;
+
+                thePlayer.followingKey.gameObject.SetActive(false);
+
+                thePlayer.followingKey = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            if(thePlayer.followingKey != null)
+            {
+                thePlayer.followingKey.followTarget = transform;
+                waitingToOpen = true;
+            }
+        }
     }
 }
