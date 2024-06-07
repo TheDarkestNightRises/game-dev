d3536027367761f2f437e84227cc0b446c2acfca
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-06-07 03:17:37 +0200

    Created paralax script

---
+++ jasper the lost twin/Assets/Scripts/UI/Parallax/ParallaxBackground.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+	//https://www.youtube.com/watch?v=wBol2xzxCOU
+	public Vector2 parallaxEffectMultiplier;
+	public bool infiniteHorizontal;
+	public bool infiniteVertical;
+ 
+	private Transform cameraTransform;
+	private Vector3 lastCameraPosition;
+	private float textureUnitSizeX;
+	private float textureUnitSizeY;
+ 
+	void Start()
+	{
+ 
+		cameraTransform = Camera.main.transform;
+		lastCameraPosition = cameraTransform.position;
+		Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+		Texture2D texture = sprite.texture;
+		textureUnitSizeX = (texture.width / sprite.pixelsPerUnit) * transform.localScale.x;
+		textureUnitSizeY = (texture.height / sprite.pixelsPerUnit) * transform.localScale.y;
+	}
+ 
+	void FixedUpdate()
+	{
+		Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+		transform.position -= new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+		lastCameraPosition = cameraTransform.position;
+ 
+		if (infiniteHorizontal)
+		{
+			if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+			{
+				float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+				transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+			}
+		}
+ 
+		if (infiniteVertical)
+		{
+			if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
+			{
+				float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+				transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
+			}
+		}
+	}
+}
+
---
+++ jasper the lost twin/Assets/Scripts/UI/Parallax/ParallaxInfinity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxInfinity : MonoBehaviour
+{
+	private float length, startPos;
+	private GameObject cam;
+	public float parallaxEffect;
+	private string cameraTag = "MainCamera";
+	
+	void Start()
+	{
+		cam = GameObject.FindWithTag(cameraTag);
+		startPos = transform.position.x;
+		length = GetComponent<SpriteRenderer>().bounds.size.x;
+	}
+	
+	void Update()
+	{
+		float temp = (cam.transform.position.x * (1 - parallaxEffect));
+		float dist = (cam.transform.position.x * parallaxEffect);
+		
+		transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+		if (temp > startPos + length) startPos += length;
+		else if (temp < startPos - length) startPos -= length;
+		
+		
+	}
+}
